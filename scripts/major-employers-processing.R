a68cb360b710f5b80052b458af2a88e55c26ecd3
jennadaly <jenna.m.daly@gmail.com>
2017-06-01 10:37:07 -0400

    Added data for 2017 profiles, fixed spelling in raw data, all tests passed.

--- scripts/major-employers-processing.R
+++ scripts/major-employers-processing.R
@@ -18,7 +18,6 @@ sub_folders <- list.files()
 data_location <- grep("raw", sub_folders, value=T)
 path_to_raw_data <- (paste0(getwd(), "/", data_location))
 ME_xlsx <- dir(path_to_raw_data, recursive=T, pattern = "MajorE") 
-ME_data_2016 <- (read_excel(paste0(path_to_raw_data, "/", ME_xlsx), sheet=1, skip=0))
 
 #bring in 2014 data only 
 legacy_ME <- dir(path_to_raw_data, recursive=T, pattern = "2014") 
@@ -81,63 +80,85 @@ ME_complete_2014_fips <- ME_complete_2014_fips %>%
   select(`Town`, `FIPS`, `Year`, `Year Submitted`, `Town Profile Year`, `Rank`, `Variable`, `Measure Type`, `Value`) %>% 
   arrange(Town, Variable, Rank)
 
+#bring in new data (for 2017 profiles)
+ME_data_2017 <- (read_excel(paste0(path_to_raw_data, "/", ME_xlsx), sheet=1, skip=0))
+#remove towns with no update
+ME_data_2017 <- ME_data_2017[!is.na(ME_data_2017$`Date Received`),]
+emp_cols <- grep("Employees", colnames(ME_data_2017))
+ME_data_2017 <- ME_data_2017[,-emp_cols]
+ME_data_2017$`Year Submitted` <- year(ME_data_2017$`Date Received`)
+ME_data_2017$`Date Received` <- NULL
+ME_data_2017$Year <- (ME_data_2017$`Year Submitted` - 1)
+ME_data_2017$`Town Profile Year` <- ME_data_2017$`Year Submitted`
+
+ME_data_2017 <- ME_data_2017 %>% 
+  select(`Town`, `Year`, `Year Submitted`, `Town Profile Year`, `1`, `2`, `3`, `4`, `5`) 
+
+#Convert to long format
+ME_2017_long <- data.frame(stringsAsFactors = F)
+# iterate through pairs of columns (ie get the name and value for rank 1 GL entry, then rank 2, etc)
+for(i in 5:9) {
+  cols = c(1, 2, 3, 4, i)
+  ranked <- ME_data_2017[, cols]
+  names(ranked) <- c("Town", "Year", "Year Submitted", "Town Profile Year", "Value")
+  ranked$Rank <- i - 4
+  ME_2017_long <- rbind(ME_2017_long, ranked)
+  remove(ranked, cols)
+}
+
+#towns names
+ME_2017_long$Town <- stri_trans_totitle(ME_2017_long$Town)
+
+#Merge in FIPS
+ME_2017_long_fips <- merge(ME_2017_long, fips, by = "Town", all=T)
+
+#remove CT 
+ME_2017_long_fips <- ME_2017_long_fips[ME_2017_long_fips$Town != "Connecticut",]
+
+#Add Measure Type and Variable
+ME_2017_long_fips$"Measure Type" <- "Name"
+ME_2017_long_fips$"Variable" <- "Employer"
+
+#Find which towns do not have 2017 data, and copy 2016 data to 2017
+no_2017 <- unique(ME_2017_long_fips[is.na(ME_2017_long_fips$`Year`),]$Town)
+#subset 2014 dataset based on these towns
+bring_from_2016 <- ME_complete_2014_fips[ME_complete_2014_fips$Town %in% no_2017,]
+bring_from_2016$`Town Profile Year` <- 2017
+
+#bind ME_2017_long_fips and bring_from_2016
+ME_2017_long_fips <- ME_2017_long_fips[!is.na(ME_2017_long_fips$Year),]
+
+ME_2017_long_fips <- ME_2017_long_fips %>% 
+  select(`Town`, `FIPS`, `Year`, `Year Submitted`, `Town Profile Year`, `Rank`, `Variable`, `Measure Type`, `Value`) 
+
+ME_complete_2017_fips <- rbind(ME_2017_long_fips, bring_from_2016)
+
+ME_complete_fips <- rbind(ME_complete_2014_fips, ME_complete_2017_fips)
+
+#Towns with non-descript values 
+#need to find these manually, no way of knowing that "We are a beach town."
+#is a non-descript answer rather than an actual employer.
+ME_incomplete <- unique(ME_complete_fips[is.na(ME_complete_fips$`Value`),]$Town)
+ME_incomplete_df <- ME_complete_fips[ME_complete_fips$Town %in% ME_incomplete,]
+#look through ME_incomplete_df to find any non-descript/ incorrect entries, 
+#if towns have incomplete ranking (less than 5) that's ok
+non_descript <- c("Easton", "Old Lyme")
+#set these towns to their 2016 values
+bring_from_2016_again <- ME_complete_2014_fips[ME_complete_2014_fips$Town %in% non_descript,]
+ME_complete_fips <- ME_complete_fips[!(ME_complete_fips$Town %in% non_descript & ME_complete_fips$Year == 2016),]
+
+ME_complete_fips <- rbind(bring_from_2016_again, ME_complete_fips)
+
+ME_complete_fips <- ME_complete_fips %>% 
+  select(`Town`, `FIPS`, `Year`, `Year Submitted`, `Town Profile Year`, `Rank`, `Variable`, `Measure Type`, `Value`) %>% 
+  arrange(Town, `Town Profile Year`, Rank)
+
+#set towns with less than 5 ranks, set rest to -9999
 # Write to File
 write.table(
-  ME_complete_2014_fips,
-  file.path(getwd(), "data", "major_employers_2014.csv"),
+  ME_complete_fips,
+  file.path(getwd(), "data", "major_employers_2014_2016.csv"),
   sep = ",",
   na = "-9999", 
   row.names = F
 )
-
-#any(is.na(ME_data_2014_fips))
-
-# #Convert to long format
-# ME_complete_long <- data.frame(stringsAsFactors = F)
-# # iterate through pairs of columns (ie get the name and value for rank 1 GL entry, then rank 2, etc)
-# for(i in 3:7) {
-#   cols = c(1, 2, i)
-#   ranked <- ME_complete[, cols]
-#   names(ranked) <- c("Year", "Town", "Value")
-#   ranked$Rank <- i - 2
-#   ME_complete_long <- rbind(ME_complete_long, ranked)
-#   remove(ranked, cols)
-# }
-# 
-# 
-# #Bring back in (old data) from 2014
-# legacy_ME <- dir(path_to_raw_data, recursive=T, pattern = "2014") 
-# old_data <- read.csv(paste0(path_to_raw_data, "/", legacy_ME), header=T, stringsAsFactors = F)
-# old_data <- old_data[old_data$Year == 2014,]
-# old_data <- old_data[!old_data$Value == "",]
-# replace_data <- old_data[old_data$Town %in% ME_na,]
-# replace_data$Variable <- gsub("Major Employer ", "", replace_data$Variable)
-# names(replace_data)[names(replace_data) == "Variable"] <- "Rank"
-# 
-# #Merge in FIPS
-# replace_data_fips <- merge(replace_data, fips, by = "Town", all.x=T)
-# 
-# #Add Measure Type and Variable
-# replace_data_fips$"Measure Type" <- "Name"
-# replace_data_fips$"Variable" <- "Employer"
-# 
-# replace_data_fips <- replace_data_fips %>% 
-#   select(`Town`, `FIPS`, `Year`, `Rank`, `Variable`, `Measure Type`, `Value`) %>% 
-#   arrange(Town, Variable, Rank)
-# 
-# #Combine old and new data
-# ME_data_combined <- rbind(ME_complete_long_fips, replace_data_fips)
-# ME_data_combined <- arrange(ME_data_combined, Town, Variable, Rank)
-# 
-# #find towns without complete top 5 (or gave non-descriptive answers)
-# ME_incomplete <- unique(ME_data_combined[is.na(ME_data_combined$`Value`),]$Town)
-# 
-# #non-descript (show all ranks as NA) 
-# #need to call these out individually, no way of knowing that "We are a beach town." 
-# #is a non-descript answer rather than an actual employer. 
-# non_descript <- c("Easton", "Old Lyme")
-# ME_data_combined$"Value"[which(ME_data_combined$Town %in% non_descript)] <- -6666
-# 
-# #all other NAs are from towns with incomplete ranks (less than 5) (set missing to -6666)
-# #("Redding", "Roxbury", "Sharon", "Warren")
-# 
